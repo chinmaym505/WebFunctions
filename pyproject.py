6b34d40333f3c5972001287c4eb0dd25184bc239
chinmaym505 <95156077+chinmaym505@users.noreply.github.com>
2022-06-26 11:58:57 -0400

    Create pyproject.py

---
+++ pyproject.py
@@ -0,0 +1,22 @@
+[build-system]
+requires = ["setuptools>=61.0"]
+build-backend = "setuptools.build_meta"
+[project]
+name = "WebFunctions"
+version = "0.0.1"
+authors = [
+  { name="Chinmay M", email="chinmay.malvania@gmail.com"},
+  { name="Ribhav S",email="ribhavksil@gmail.com"}
+]
+description = ""
+readme = "README.md"
+requires-python = ">=3.7"
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+[project.urls]
+"Homepage" = "https://github.com/pypa/sampleproject"
+"Bug Tracker" = "https://github.com/pypa/sampleproject/issues"
